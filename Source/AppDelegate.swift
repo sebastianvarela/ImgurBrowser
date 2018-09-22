10455fba47c9e2408d5665b1ea5e1819e1172470
Sebastián Varela <sebastian.varela@s3ba.net>
2018-09-22 14:45:23 +0200

    Add empty project

---
+++ Source/AppDelegate.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+@UIApplicationMain
+public class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    public var window: UIWindow?
+
+    public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    public func applicationWillResignActive(_ application: UIApplication) {
+    }
+
+    public func applicationDidEnterBackground(_ application: UIApplication) {
+    }
+
+    public func applicationWillEnterForeground(_ application: UIApplication) {
+    }
+
+    public func applicationDidBecomeActive(_ application: UIApplication) {
+    }
+
+    public func applicationWillTerminate(_ application: UIApplication) {
+    }
+}
---
+++ Tests/ImgurBrowserTests.swift
@@ -0,0 +1,26 @@
+@testable import ImgurBrowser
+import XCTest
+
+public class ImgurBrowserTests: XCTestCase {
+    public override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    public override func tearDown() {
+        super.tearDown()
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    public func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    public func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+}
